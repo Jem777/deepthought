bcfddad5f099098a915a1cdc2a789ca5d66a9a5e
Jem777 <Clifford_Burton@web.de>
2009-11-22 00:21:37 +0100

    improved showxml

--- ShowXml.hs
+++ ShowXml.hs
@@ -7,9 +7,9 @@ class ShowXml a where
     showxml :: a -> String
 
 instance ShowXml Expression where
-    showxml (Variable str) = "<variable>\"" ++ str ++ "\"</variable>"
-    showxml (Application fun args) = "<application><function>" ++ (show fun) ++"</function><arguments>" ++ concat (map show args) ++ "</arguments></application>"
-    showxml (Datatype t) = "\"" ++ show t ++ "\""
+    showxml (Variable str) = "<variable>" ++ str ++ "</variable>"
+    showxml (Application fun args) = "<application><function>" ++ (showxml fun) ++"</function><arguments>" ++ concat (map showxml args) ++ "</arguments></application>"
+    showxml (Datatype t) = showxml t 
     showxml Wildcard = "wildcard"
 
 instance ShowXml Datatype where
@@ -17,9 +17,9 @@ instance ShowXml Datatype where
     showxml (Tupel expr) = "<list>" ++ concat (map showxml expr) ++ "</list>"
     showxml (Number int) = "<number>" ++ show int ++ "</number>"
     showxml (Float fl) = "<float>" ++ show fl ++ "</float>"
-    showxml (String str) = "<list>" ++ concat (map showxml str) ++ "</list>"
-    showxml (Char char) = "<char>"
-    showxml (Atom str) = "<atom>"
-    showxml (Operator op) = "<operator>"
-    showxml (Lambda (p:[]) expr) = "<lambda>" ++ concat (map showxml expr ++ "</lambda>"
-    showxml (Lambda (p:ps) expr) = "<lambda>" ++ concat (map show patr) ++ "</lambda>"
+    showxml (String str) = "<list>" ++ concat (map (showxml . Char) str) ++ "</list>"
+    showxml (Char char) = "<char>" ++ char:[] ++ "</char>"
+    showxml (Atom str) = "<atom>" ++ str ++ "</atom>"
+    showxml (Operator op) = "<operator>" ++ op ++ "</operator>"
+    showxml (Lambda (p:[]) expr) = "<lambda><pattern>" ++ (showxml p) ++ "</pattern><expression>" ++ (showxml expr) ++ "</expression></lambda>"
+    showxml (Lambda (p:ps) expr) = "<lambda><pattern>" ++ (showxml p) ++ "</pattern><expression>" ++ showxml (Lambda ps expr) ++ "</expression></lambda>"
