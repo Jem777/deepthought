052b4b8f9029387d04550175622a0bec000dfb65
Jem777 <Clifford_Burton@web.de>
2010-07-30 23:33:14 +0200

    added state to the builtin-functions

--- src/Misc.hs
+++ src/Misc.hs
@@ -3,3 +3,13 @@ module Misc where
 import Data.Either (either, lefts, rights)
 
 eitherRight f = either Left (Right . f) 
+
+mapEither f (Right x) (Right y) = Right (f x y)
+mapEither _ (Left x) (Left y) = Left (x ++ y)
+mapEither _ (Left x) _ = Left x
+mapEither _ _ (Left x) = Left x
+
+eitherFold _ _ [] = Right []
+eitherFold f g l
+    | (not . null) (lefts l) = Left (foldl1 f (lefts l))
+    | otherwise = Right (foldl1 g (rights l))
--- src/StdLib.hs
+++ src/StdLib.hs
@@ -1,11 +1,16 @@
 module StdLib where
 
+-- System Imports
+import Data.List
+import Data.Either (lefts, rights, either)
+import Data.Maybe (fromJust)
+import Control.Monad (ap, mapM, foldM)
+import Control.Applicative ((<$>), (<*>))
+
+-- Deepthought Imports
 import Types
 import Errors
-import Data.List
-import Data.Either (lefts, rights)
-import Data.Maybe (fromJust)
-import Control.Monad (ap, mapM)
+import Misc
 
 -- the functions
 
@@ -13,78 +18,97 @@ import Control.Monad (ap, mapM)
 
 --add :: SourcePos -> Datatype -> Datatype -> Either [CompileError] Datatype
 --add _ [x] = goRight (Lambda foo bar)
-add _ [List a, List b] = goRight (List (a ++ b))
-add _ [Tupel a, Tupel b] = goLeft [CompileError "TypeError" testEmptyPos ""] --tupelAdd a b
-add _ [Number a, Number b] = goRight (Number (a + b))
-add _ [Float a, Number b] = goRight (Float (a + (fromInteger b)))
-add _ [Number a, Float b] = goRight (Float ((fromInteger a) + b))
-add _ [Float a, Float b] = goRight (Float (a + b))
-add _ [String a, String b] = goRight (String (a ++ b))
-add pos x | (length x) > 2 = goLeft [tooMuchArguments pos "+" 2 (length x)]
-          | otherwise = goLeft [typeException pos "+" x]
+add _ _ [List a, List b] = goRight (List (a ++ b))
+--add pos state [Tupel a, Tupel b] = tuplef pos state add a b --goLeft [CompileError "TypeError" testEmptyPos ""] --tupelAdd a b
+add _ _ [Number a, Number b] = goRight (Number (a + b))
+add _ _ [Float a, Number b] = goRight (Float (a + (fromInteger b)))
+add _ _ [Number a, Float b] = goRight (Float ((fromInteger a) + b))
+add _ _ [Float a, Float b] = goRight (Float (a + b))
+add _ _ [String a, String b] = goRight (String (a ++ b))
+add pos _ x
+        | (length x) > 2 = goLeft [tooMuchArguments pos "+" 2 (length x)]
+        | otherwise = goLeft [typeException pos "+" x]
 
 --sub :: SourcePos -> Datatype -> Datatype -> Either [CompileError] Datatype
-sub _ [List a, List b] = goRight (List (a \\ b)) -- has the be evaled first
-sub _ [Tupel a, Tupel b] = goLeft [CompileError "TypeError" testEmptyPos ""]
-sub _ [Number a, Number b] = goRight (Number (a - b))
-sub _ [Float a, Number b] = goRight (Float (a - (fromInteger b)))
-sub _ [Number a, Float b] = goRight (Float ((fromInteger a) - b))
-sub _ [Float a, Float b] = goRight (Float (a - b))
-sub _ [String a, String b] = goRight (String (a \\ b))
-sub pos x | (length x) > 2 = goLeft [tooMuchArguments pos "-" 2 (length x)]
-          | otherwise = goLeft [typeException pos "-" x]
+sub _ _ [List a, List b] = goRight (List (a \\ b)) -- has the be evaled first
+sub _ _ [Tupel a, Tupel b] = goLeft [CompileError "TypeError" testEmptyPos ""]
+sub _ _ [Number a, Number b] = goRight (Number (a - b))
+sub _ _ [Float a, Number b] = goRight (Float (a - (fromInteger b)))
+sub _ _ [Number a, Float b] = goRight (Float ((fromInteger a) - b))
+sub _ _ [Float a, Float b] = goRight (Float (a - b))
+sub _ _ [String a, String b] = goRight (String (a \\ b))
+sub pos _ x
+        | (length x) > 2 = goLeft [tooMuchArguments pos "-" 2 (length x)]
+        | otherwise = goLeft [typeException pos "-" x]
 
-neg :: SourcePos -> [Datatype] -> IO (Either [CompileError] Datatype)
-neg _ [Number a] = goRight (Number (negate a))
-neg _ [Float a] = goRight (Float (negate a))
-neg pos x | (length x) > 1 = goLeft [tooMuchArguments pos "negate" 1 (length x)]
-          | otherwise = goLeft [typeException pos "negate" x]
+neg :: SourcePos -> State -> [Datatype] -> IO (Either [CompileError] Datatype)
+neg _ _ [Number a] = goRight (Number (negate a))
+neg _ _ [Float a] = goRight (Float (negate a))
+neg pos _ x
+        | (length x) > 1 = goLeft [tooMuchArguments pos "negate" 1 (length x)]
+        | otherwise = goLeft [typeException pos "negate" x]
 
-mul _ [List a, Number b] = goRight (List (concat (replicate (fromInteger b) a)))
-mul _ [Tupel a, Number b] = goLeft [CompileError "TypeError" testEmptyPos ""] -- has to be evaled first
-mul _ [Number a, Number b] = goRight (Number (a * b))
-mul _ [Float a, Number b] = goRight (Float (a * (fromInteger b)))
-mul _ [Number a, Float b] = goRight (Float ((fromInteger a) * b))
-mul _ [Float a, Float b] = goRight (Float (a * b))
-mul _ [String a, Number b] = goRight (String (concat (replicate (fromInteger b) a)))
-mul pos x | (length x) > 2 = goLeft [tooMuchArguments pos "*" 2 (length x)]
-          | otherwise = goLeft [typeException pos "*" x]
+mul _ _ [List a, Number b] = goRight (List (concat (replicate (fromInteger b) a)))
+mul _ _ [Tupel a, Number b] = goLeft [CompileError "TypeError" testEmptyPos ""] -- has to be evaled first
+mul _ _ [Number a, Number b] = goRight (Number (a * b))
+mul _ _ [Float a, Number b] = goRight (Float (a * (fromInteger b)))
+mul _ _ [Number a, Float b] = goRight (Float ((fromInteger a) * b))
+mul _ _ [Float a, Float b] = goRight (Float (a * b))
+mul _ _ [String a, Number b] = goRight (String (concat (replicate (fromInteger b) a)))
+mul pos _ x
+        | (length x) > 2 = goLeft [tooMuchArguments pos "*" 2 (length x)]
+        | otherwise = goLeft [typeException pos "*" x]
 
-division _ [Number a, Number b] = goRight (Float ((fromInteger a) / (fromInteger b)))
-division _ [Float a, Number b] = goRight (Float (a / (fromInteger b)))
-division _ [Number a, Float b] = goRight (Float ((fromInteger a) / b))
-division _ [Float a, Float b] = goRight (Float (a / b))
-division pos x | (length x) > 2 = goLeft [tooMuchArguments pos "/" 2 (length x)]
-               | otherwise = goLeft [typeException pos "/" x]
+division _ _ [Number a, Number b] = goRight (Float ((fromInteger a) / (fromInteger b)))
+division _ _ [Float a, Number b] = goRight (Float (a / (fromInteger b)))
+division _ _ [Number a, Float b] = goRight (Float ((fromInteger a) / b))
+division _ _ [Float a, Float b] = goRight (Float (a / b))
+division pos _ x
+        | (length x) > 2 = goLeft [tooMuchArguments pos "/" 2 (length x)]
+        | otherwise = goLeft [typeException pos "/" x]
 
-printf _ [Number a] = do_io print a
-printf _ [String a] = do_io print a
-printf _ [Float a] = do_io print a
-printf _ [Char a] = do_io print a
-printf _ [Atom a] = do_io putStrLn a
-printf pos x | (length x) > 1 = goLeft [tooMuchArguments pos "print" 1 (length x)]
-             | otherwise = goLeft [typeException pos "print" x]
+listconstructor pos _ [a, List b] = goRight (List ((Datatype pos a):b))
+listconstructor pos _ x
+        | (length x) > 2 = goLeft [tooMuchArguments pos ":" 2 (length x)]
+        | otherwise = goLeft [typeException pos ":" x]
 
-printStr _ [String a] = do_io putStrLn a
-printStr pos x | (length x) > 1 = goLeft [tooMuchArguments pos "printStr" 1 (length x)]
-             | otherwise = goLeft [typeException pos "printStr" x]
+printf pos state [List a] = eitherFold (++)
+printf _ _ [Number a] = do_io print a
+printf _ _ [String a] = do_io print a
+printf _ _ [Float a] = do_io print a
+printf _ _ [Char a] = do_io print a
+printf _ _ [Atom a] = do_io putStrLn a
+printf pos _ x
+        | (length x) > 1 = goLeft [tooMuchArguments pos "print" 1 (length x)]
+        | otherwise = goLeft [typeException pos "print" x]
+
+printStr _ _ [String a] = do_io putStrLn a
+printStr pos _ x
+        | (length x) > 1 = goLeft [tooMuchArguments pos "printStr" 1 (length x)]
+        | otherwise = goLeft [typeException pos "printStr" x]
 
 do_io f x = f x >> goRight (Atom "@ok")
---listconstructor ()
 
 
+
+simplify :: State -> (Expression, Expression) -> IO (Either [CompileError] [(String, Datatype)])
+simplify state (Variable _ string, expression) =
+    (eval state expression) >>= 
+    (return . (either Left (\datatype -> Right [(string, datatype)])))
+simplify state (pattern, application) = goLeft []
+
+--eval state (Application pos (Lambda x y) args) = lambda state pos x y args
 eval state (Application pos fun args) = apply (translateFun fun) state pos fun args
 eval _ (Datatype _ x) = goRight x
 eval state (Variable pos str)
-    | getVariable state (Variable pos str) == Nothing = goLeft [CompileError "asdf" pos ("variable " ++ str ++ " unbound")]
-    | otherwise = (goRight . fromJust) (getVariable state (Variable pos str))
+    | getVariable state str == Nothing = goLeft [CompileError "asdf" pos ("variable " ++ str ++ " unbound")]
+    | otherwise = (goRight . fromJust) (getVariable state str)
 eval a b = goRight (Atom "foo")
-
 
 apply Nothing _ p fun _ = goLeft [nameException p fun]
 apply (Just f) state pos fun args = (apply2 pos) state f args
 
-apply2 pos state fun args = (mapM evaluate args) >>= help (fun pos)
+apply2 pos state fun args = (mapM evaluate args) >>= help (fun pos state)
     where
     evaluate x = (return x) >>= (eval state)
     help f l
@@ -96,16 +120,24 @@ translateFun fun = lookup (value fun) functionList
 pureFunctions = [("+", add), ("-", sub), ("neg", neg), ("*", mul), ("/", division)]
 impureFunctions = [("print", printf), ("printStr", printStr)]
 functionList = (pureFunctions ++ impureFunctions)
---functionList = [("neg", neg)]
 
 -- internal functions
 
 goRight = return . Right
 goLeft = return . Left
 
-tupelAdd [] [] = Right (Tupel [])
-tupelAdd a b | (length a) == (length b) = Right (Tupel (a ++ b))
-             | otherwise = Left (CompileError "TypeError" testEmptyPos "length of the tupels don't match")
+--tuplef _ _ _ [] = goRight (Tupel [])
+--tuplef pos state f argMatrix = foldM help (Right (Tupel [])) (map (\argList -> mapM (eval state) argList >>= (eFold (f pos state))) (transpose argMatrix))
+--    where
+
+--help :: (Monad m) => Either [a] Datatype -> m (Either [a] Datatype) -> m (Either [a] Datatype)
+--help x y = y >>= (mapEither (\a b -> Tupel ((Datatype a):b)) x)
+
+--eitherFold _ [] = Right []
+eFold f l
+    | (not . null) (lefts l) = goLeft (foldl1 (++) (lefts l))
+    | otherwise = (f (rights l))
 
 
 --reportTypeError :: String -> [Datatype] -> CompileError
+
