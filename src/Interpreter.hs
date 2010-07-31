af1e74b40f37594b63e491d64735195cbce5b782
Jem777 <Clifford_Burton@web.de>
2010-08-01 00:32:02 +0200

    improved Interpreter.run

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -8,9 +8,10 @@ import Types
 import StdLib
 import Expr
 
-main = runEitherErr (run "3+3") >>= either print print
+--main = runEitherErr (run "3+3") >>= either print print
+main = run "3+3"
 
 run code = f ((testparse expression) "" code)
     where
-    f (Right x) = eval testEmptyState x
-    f (Left x) = goLeft [formatError x]
+    f (Right x) = runEitherErr (eval testEmptyState x) >>= either print print
+    f (Left x) = print [formatError x]
