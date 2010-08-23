b36d2f76029023dec416758d4f76edf1d9049b03
Jem777 <Clifford_Burton@web.de>
2010-08-23 22:08:01 +0200

    interpreter prints in a better way

--- src/Interpreter.hs
+++ src/Interpreter.hs
@@ -13,5 +13,7 @@ main = run "3+3"
 
 run code = f ((testparse expression) "" code)
     where
-    f (Right x) = runEitherErr (eval testEmptyState x) >>= either print print
+    f (Right x) =
+        runEitherErr (eval testEmptyState x >>= prettyShow testEmptyState) >>=
+        either print putStrLn
     f (Left x) = print [formatError x]
