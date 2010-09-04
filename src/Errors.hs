d1852ac2afb33b324d5984f97d5cbeb69e2f168b
Jem777 <Clifford_Burton@web.de>
2010-09-04 23:05:29 +0200

    added patternException

--- src/Errors.hs
+++ src/Errors.hs
@@ -8,7 +8,7 @@ varUnbound x = CompileError "NameError" (position x) ("Variable " ++ (show x) ++
 
 --runtime exceptions
 nameException pos opName = CompileError "NameError" pos ("function " ++ (value opName) ++ " not found")
-typeException :: SourcePos -> String -> [Datatype] -> CompileError
 typeException pos opName list = CompileError "TypeError" pos ("unsupported types for " ++ opName ++ ": " ++ (foldl1 (\x y -> x ++ " and " ++ y) (map show list)))
 tooMuchArguments pos opName exspected got =
     CompileError "TypeError" pos (opName ++ " takes at most " ++ (show exspected) ++ " arguments (" ++ (show got) ++ " given)")
+patternException expr = CompileError "PatternException" (position expr) "Couldn't match arguments against pattern"
